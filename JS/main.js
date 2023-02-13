7cc6add4173bf55d47a7eb3fb8961d0b0a8d63d9
Fernando1999hsm <fernandocalva15@gmail.com>
2023-02-12 23:29:21 -0600

    Insertar y mostrar datos, finalizado

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,6 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
+import { getDatabase , ref, set , onValue } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-database.js";
+/* INICIANDO Y CONFIGURANDO FIREBASE*/
 const firebaseConfig = {
   apiKey: "AIzaSyCJJlwaKb3DJNvu2rYPxyNus0cdTn-l30A",
   authDomain: "crud-javascrit.firebaseapp.com",
@@ -15,32 +11,76 @@ const firebaseConfig = {
   appId: "1:909419132537:web:57bc741dc14954fbf986a8",
   measurementId: "G-SGQP7B3XEX"
 };
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
+const AccesDB = getDatabase(app);
+/*VARIABLES PARA ABRIR/CERRAR EL MODAL*/
 const openModal = document.getElementById('openRegisterModal')
 const modal = document.getElementById('modal')
 const closeModal = document.getElementById('close-modal')
 const registerForm = document.getElementById('register-form')
-
-showRegisterModal = ()=>{
-    modal.classList.toggle('is-active')
+/*ID DE LA TABLA*/
+const studentsTabla = document.getElementById('CuerpoTabla')
+/*VARIABLE PARA ID DE ESTUDIANTES*/
+let idStudent = 0;
+/*FUNCION PARA MOSTRAR DATOS*/
+window.addEventListener('DOMContentLoaded', async(e)=>{
+  const starCountRef = await ref(AccesDB, 'students/');
+  onValue(starCountRef, (snapshot) => {
+    studentsTabla.innerHTML = ''
+    const data = snapshot.val();
+    data.forEach(element => {
+      /*Checnado ID*/
+      if(element.Id>idStudent){idStudent=element.Id}
+      studentsTabla.innerHTML += '<tr>'+
+      '<th>1</th>'+
+      '<td>'+element.Name+'</td>'+
+      '<td>'+element.ApPaterno+'</td>'+
+      '<td>'+element.ApMaterno+'</td>'+
+      '<td>'+element.Telefono+'</td>'+
+      '<td>'+element.Correo+'</td>'+
+      '<td>'+
+        '<div class="btn-operaciones">'+
+          '<button class="button is-warning" data-id='+'>'+
+            '<i class="fas fa-pencil-alt"></i>'+
+          '</button>'+
+          '<button class="button is-danger">'+
+            '<i class="fas fa-trash"></i>'+
+          '</button>'+
+        '</div>'+
+      '</td>'+
+      '</tr>'
+    })
+  })
+})
+/*FUNCION DEL MODAL*/
+const showRegisterModal = ()=>{
+  modal.classList.toggle('is-active')
 }
-
+/*fUNCION DE LOS MODALES*/
 openModal.addEventListener('click',showRegisterModal)
 closeModal.addEventListener('click',showRegisterModal)
 
-registerForm.addEventListener('submit',(e)=>{
-    e.preventDefault()
+/*FUNCION PARA REGISTRAR ESTUDIANTES*/
+registerForm.addEventListener('submit', (e)=>{
+  e.preventDefault()
 
-    const nombre = registerForm['nombre'].value
-    const apellidoPaterno = registerForm['apellido-paterno'].value
-    const apellidoMaterno = registerForm['apellido-materno'].value
-    const teleono = registerForm['telefono'].value
-    const correo = registerForm['correo'].value
-    const descripcion = registerForm['descripcion'].value
+  const nombre = registerForm['nombre'].value
+  const apellidoPaterno = registerForm['apellido-paterno'].value
+  const apellidoMaterno = registerForm['apellido-materno'].value
+  const teleono = registerForm['telefono'].value
+  const correo = registerForm['correo'].value
+  const descripcion = registerForm['descripcion'].value
 
-    app.database
+  set(ref(AccesDB,'students/'+ (idStudent+1)),{
+    Id:idStudent,
+    Name: nombre,
+    ApPaterno:apellidoPaterno,
+    ApMaterno:apellidoMaterno,
+    Telefono:teleono,
+    Correo:correo,
+    Desc:descripcion
+  })
+  showRegisterModal()
+
 })
+
